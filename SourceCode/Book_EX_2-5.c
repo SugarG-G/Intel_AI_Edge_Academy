59053c330597d3a12153e4f220eca7cef6cb70ce
chi <cldud97@naver.com>
2025-04-08 14:55:38 +0900

    Book_EX_2-4 ~ 2.7 ADD

---
+++ SourceCode/Book_EX_2-5.c
@@ -0,0 +1,19 @@
+/* 
+    작성자 : 윤치영
+    작성일 : 2025.04.08. AM 14:30:00
+    제목 : 3가지 진법의 정수 상수
+*/
+
+#include <stdio.h>
+
+int main(){
+    printf("%d\n", 12);
+    printf("%d\n", 014);
+    printf("%d\n", 0xc);
+
+    printf("%o\n", 100);
+    printf("%x\n", 100);
+    printf("%X\n", 100);
+
+    return 0;
+}
