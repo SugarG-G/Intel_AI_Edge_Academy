9d2ce8bed97cd3993390524e3647a21cde884366
chi <cldud97@naver.com>
2025-04-08 12:59:29 +0900

    Book_EX 2-1~2-3 ADD

---
+++ SourceCode/Book_EX_2-2.c
@@ -0,0 +1,14 @@
+/* 
+    작성자 : 윤치영
+    작성일 : 2025.04.08. AM 12:47:00
+    제목 : 문자열을 화면에 출력하는 프로그램
+*/
+
+#include <stdio.h>
+
+int main(){
+    printf("Be Happy");
+    printf("My friend");
+
+    return 0;
+}
