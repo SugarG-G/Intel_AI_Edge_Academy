cd8baa7d7136cebdc357d0b4654ca482f23433d2
chi <cldud97@naver.com>
2025-04-08 16:23:16 +0900

    .

---
+++ SourceCode/Book_Chapter_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    // 15 > 20, 5 > 6 º¯°æ
+    printf("%d + %d = %d\n", 20, 6, 20 + 6);
+    printf("%d - %d = %d\n", 20, 6, 20 - 6);
+    printf("%d * %d = %d\n", 20, 6, 20 * 6);
+    printf("%d / %d = %d\n", 20, 6, 20 / 6);
+
+    return 0;
+}
