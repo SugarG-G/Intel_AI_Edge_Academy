d3e85459f2b19e580d531cfa8c45a12884acff7a
chi <cldud97@naver.com>
2025-04-11 15:49:26 +0900

    .

--- SourceCode/C_Book_Chapter_8.c
+++ SourceCode/C_Book_Chapter_8.c
@@ -1,5 +1,5 @@
 /***********************************************************/
-// [8-0] 배열의 선언과 사용
+// [8-1] 배열의 선언과 사용
 /***********************************************************/
 
 #if 0
@@ -25,10 +25,10 @@ int main(void)
 #endif
 
 /***********************************************************/
-// [8-1] 배열의 선언과 사용
+// [8-2] 배열의 선언과 사용
 /***********************************************************/
 
-#if 1
+#if 0
 #include <stdio.h>
 
 int main(void)
@@ -47,7 +47,7 @@ int main(void)
 #endif
 
 /***********************************************************/
-// [0-0] 타이틀
+// [8-3] sizeof 연산자를 사용한 배열
 /***********************************************************/
 
 #if 0
@@ -55,13 +55,25 @@ int main(void)
 
 int main(void)
 {
-	
+    int arr[5];
+    int num;
+    int result = 0;
+	for(int i = 0; i < 5; i++){
+        printf("%d과목 성정을 입력하세요 : ", i+1);
+        scanf("%d", &num);
+        arr[i] = num;
+        result += arr[i];
+    }
+
+    printf("총점 : %d점, ", result);
+    printf("평균 : %d점", result/(sizeof(arr) / sizeof(int)));
+
 	return 0;
 }
 #endif
 
 /***********************************************************/
-// [0-0] 타이틀
+// [8-4] char 형 배열의 선언과 초기화
 /***********************************************************/
 
 #if 0
@@ -69,35 +81,76 @@ int main(void)
 
 int main(void)
 {
-	
+    //배열에 문자열을 저장할경우 null (\0)을 포함한 크기를 지정해줄것
+	int array[5] = { 1, 2, 3, 4, 5};
+    char fruit[6] = {'a', 'p', 'p', 'l', 'e'};
+    char fruit2[6] = "apple";
+
+    char my_name[10];
+    my_name[0] = 'c';
+    my_name[1] = 'h';
+    my_name[2] = 'i';
+    my_name[3] = '\0';
+
+    printf("%s", my_name);
+    /*
+    for(int i = 0 ; i < 5; i++){
+        printf("%c", fruit[i]);
+    }
+    printf("\n");
+    printf("%s", fruit2);
+    */
+    
 	return 0;
 }
 #endif
 
 /***********************************************************/
-// [0-0] 타이틀
+// [8-5] 문자열을 대입하는 strcpy 함수
 /***********************************************************/
 
 #if 0
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-	
+    char fruit[100] = "apple";
+    printf("현재 과일 : %s\n", fruit);
+    
+    //strcpy(이전 문자열, 새로운 문자열);
+    strcpy(fruit, "banana");
+    printf("새로운 과일 : %s\n", fruit);
+
+
+    char cat_name[100] = "이름을 지어주세요 : ";
+    char new_name[100];
+    
+    printf("%s", cat_name);
+    scanf("%s", new_name);
+    strcpy(cat_name, new_name);
+    printf("%s", cat_name);
 	return 0;
 }
 #endif
 
 /***********************************************************/
-// [0-0] 타이틀
+// [8-6] gets(), puts() 띄어쓰기까지 입력받기
 /***********************************************************/
 
 #if 0
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-	
+    char inputs[100];
+
+    printf("내일의 점심 메뉴 입력 : ");
+    gets(inputs);
+
+    puts(inputs);
+
 	return 0;
 }
 #endif
