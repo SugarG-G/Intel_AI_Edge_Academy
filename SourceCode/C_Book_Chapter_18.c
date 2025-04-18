595342f266ac02b529c79111fc05c5ba7987bdfd
chi <cldud97@naver.com>
2025-04-18 14:05:29 +0900

    .

--- SourceCode/C_Book_Chapter_18.c
+++ SourceCode/C_Book_Chapter_18.c
@@ -69,7 +69,7 @@ int main(void)
 // [18-3-연습] 파일 입출력
 /***********************************************************/
 
-#if 1
+#if 0
 #include <stdio.h>
 #include <string.h>
 
@@ -150,6 +150,129 @@ int main(void)
     fclose(fpb);
     fclose(fpc);
 
+    return 0;
+}
+#endif
+
+/***********************************************************/
+// [18-4, 5] 표준 입출력, stdin, stdout
+/***********************************************************/
+
+#if 0
+#include <stdio.h>
+
+int main(void)
+{
+    int ch;
+    while (1)
+    {
+        ch = getchar();
+        if (ch == EOF)
+        {
+            break;
+        }
+        putchar(ch);
+    }
+
+    while(1){
+        ch = fgetc(stdin); //현재 OS와 연결된 키보드
+        if(ch == EOF) break;
+        fputc(ch, stdout);
+    }
+
+    return 0;
+}
+#endif
+
+/***********************************************************/
+// [18-6] 파일 형태와 개방 모드가 다른 경우
+/***********************************************************/
+
+#if 0
+#include <stdio.h>
+
+int main(void)
+{
+	FILE *pf;
+    const char *path = "D:\\Intel_AI_Edge_Acadamy\\d.txt";
+
+    fopen(path, "wb");
+    if(pf == NULL) return 1;
+
+    int array[10] = { 13, 10, 13, 13, 10, 26, 13, 10, 13, 10 };
+
+    for(int i = 0; i < 10; i++){
+        fputc(array[i], pf);
+    
+    }
+    fclose(pf);
+
+    // txt 형식으로 읽어보자
+    pf = fopen(path, "r");
+
+    int ch;
+    while(1){
+        ch = fgetc(pf);
+        if(ch == EOF) break;
+        printf("%3d", ch);
+    }
+
+    fclose(pf);
+	return 0;
+}
+#endif
+
+/***********************************************************/
+// [18-7] a+ 모드로 파일 내용 확인하며 출력
+/***********************************************************/
+
+#if 1
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    FILE *fp;
+    const char *path = "D:\\Intel_AI_Edge_Acadamy\\music.txt";
+    char str[20];
+
+    fp = fopen(path, "a+");
+    if (fp == NULL)
+        return 1;
+
+    // logic
+
+    while (1)
+    {
+        printf("노래 제목 : "); // 아몬드 초콜릿
+        scanf("%s", str);
+        if (strcmp(str, "end") == 0)
+            break;                         // end <- 프로그램 종료
+        else if (strcmp(str, "list") == 0) // list <- 현재까지 저장된 모든 노래 출력!!
+        {
+            // 모든 정보 출력하기 전
+            // fp를 맨 첫 줄
+            // 맨 앞으로 이동시키자
+            fseek(fp, 0, SEEK_SET);
+            while (1)
+            {
+                // 개행(\n)가져오기 위해 +1
+                fgets(str, strlen(str) + 1, fp);
+                // 행 끝에 도달하면
+                // feof() ->
+                if (feof(fp) != 0)
+                    break;
+                printf("%s", str);
+            }
+        }
+        // while(1) fputc(), -> 문장 전체를 입력, fputs() fprintf
+        else
+            fprintf(fp, "%s\n", str);
+        // str에 들어 있는 노래 정보를
+        // 맨 끝에 d.txt에 쓰자!!
+    }
+
+    fclose(fp); // 열었으면 닫자, 열었으면 닫자!!
     return 0;
 }
 #endif
