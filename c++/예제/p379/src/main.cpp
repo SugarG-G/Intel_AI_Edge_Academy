e54dc088d7577ebe6904c424960372554ec5d56d
chi <cldud97@naver.com>
2025-07-04 11:26:58 +0900

    .

--- c++/예제/p379/src/main.cpp
+++ c++/예제/p379/src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
+
+#define apply
 
 class Point
 {
@@ -15,6 +16,11 @@ public:
         this->y = y;
     }
     void showPoint() { cout << "(" << x << "," << y << ")" << endl; }
+    virtual void drivedShow()
+    {
+        cout << "drivedShow" << endl;
+        showPoint();
+    }
 };
 
 class ColorPoint : public Point
@@ -24,21 +30,72 @@ private:
 
 public:
     void setColor(string color) { this->color = color; }
-    void showColorPoint();
-    void showPoint(); // 재정의
+    virtual void drivedShow();
+    // void showPoint(); // 재정의
 };
 
-void ColorPoint::showColorPoint()
+void ColorPoint::drivedShow()
 {
     cout << color << ":";
     showPoint();
 }
 
-void ColorPoint::showPoint() // 재정의
+// void ColorPoint::showPoint() // 재정의
+// {
+//     cout << "재정의 " << "(" << x << "," << y << ")" << endl;
+// }
+#ifdef apply
+// 응용 //
+class LinePoint : public Point
 {
-    cout << "재정의 " << "(" << x << "," << y << ")" << endl;
+private:
+    string line;
+
+public:
+    void setLine(string line) { this->line = line; }
+    virtual void drivedShow();
+    // void showPoint(); // 재정의
+};
+
+void LinePoint::drivedShow()
+{
+    cout << line << ":";
+    showPoint();
 }
 
+// void LinePoint::showPoint() // 재정의
+// {
+//     cout << "재정의 " << "(" << x << "," << y << ")" << endl;
+// }
+// 응용 end //
+#endif
+#ifdef apply
+// 응용 //
+void testshow(Point *p)
+{
+    cout << "----------testshow---------" << endl;
+    // p->set(7, 9);
+    // p->showPoint();
+    // cout << typeid(ColorPoint).name() << endl;
+    // cout << typeid(*p).name() << endl;
+
+    // if (typeid(*p) == typeid(ColorPoint))
+    // {
+    //     ColorPoint *pDer = (ColorPoint *)p;
+    //     cout << "pDer->showColorPoint()" << endl;
+    //     pDer->showColorPoint();
+    // }
+    // else if (typeid(*p) == typeid(LinePoint))
+    // {
+    //     LinePoint *pLin = (LinePoint *)p;
+    //     cout << "LinePoint *pLin = (LinePoint *)p" << endl;
+    //     pLin->showLinePoint();
+    // }
+    p->drivedShow();
+    cout << "----------testshow---------" << endl;
+}
+// 응용 end //
+#endif
 int main()
 {
     // Point p;
@@ -48,14 +105,35 @@ int main()
     // cp.showColorPoint();
 
     // 업 캐스팅 예제
+    // ColorPoint cp;
+    // ColorPoint *pDer = &cp;
+
+    // Point *pBase = pDer; // <- 업캐스팅, Point *pBase = (Point *)pDer;
+    // pBase->set(3, 4);
+    // pBase->showPoint();
+
+    // // 다운 캐스팅 예제
+    // pDer = (ColorPoint *)pBase; // 다운 캐스팅
+    // pDer->setColor("Red");
+    // pDer->showColorPoint();
+#ifdef apply
+    // 응용 //
     ColorPoint cp;
     ColorPoint *pDer = &cp;
+    pDer->set(3, 4);
+    pDer->setColor("red");
+    pDer->drivedShow();
 
-    Point *pBase = pDer; // <- 업캐스팅, Point *pBase = (Point *)pDer;
-    pBase->set(3, 4);
-    pBase->showPoint();
+    testshow(pDer);
 
-    pDer = (ColorPoint *)pBase; // 다운 캐스팅
-    pDer->setColor("Red");
-    pDer->showColorPoint();
+    cout << "---------------------------------" << endl;
+
+    LinePoint lp;
+    LinePoint *pLin = &lp;
+    pLin->set(5, 7);
+    pLin->setLine("test");
+    pLin->drivedShow();
+    testshow(pLin);
+    // 응용 end //
+#endif
 }
