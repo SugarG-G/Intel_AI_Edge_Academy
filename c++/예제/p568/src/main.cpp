fcf950512fe63a0172a00cd00647e57c15ddecf0
chi <cldud97@naver.com>
2025-07-07 14:34:17 +0900

    예제 11-10

---
+++ c++/예제/p568/src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Book
+{
+    string title;
+    string press;
+    string author;
+
+public:
+    Book(string title = "", string press = "", string author = "")
+    {
+        this->title = title;
+        this->author = author;
+        this->press = press;
+    }
+    friend ostream &operator<<(ostream &stream, Book b);
+};
+
+ostream &operator<<(ostream &stream, Book b)
+{
+    stream << b.title << "," << b.press << "," << b.author;
+    return stream;
+}
+
+int main()
+{
+    Book book("소유냐 존재냐", "한국출판사", "에리히프롬");
+    cout << book;
+}
