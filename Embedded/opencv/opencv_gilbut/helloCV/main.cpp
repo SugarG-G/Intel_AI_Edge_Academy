961f56e08f309463be20b3fd07535ae75e382f47
chi <cldud97@naver.com>
2025-07-23 11:19:05 +0900

    opencv 코드

---
+++ Embedded/opencv/opencv_gilbut/helloCV/main.cpp
@@ -0,0 +1,31 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	cout << "Hello OpenCV " << CV_VERSION << endl;
+
+	Mat img;
+	img = imread("opencv_gilbut\\helloCV\\lenna.bmp");
+
+	if (img.empty()) {
+		cerr << "Image load failed!" << endl;
+		return -1;
+	}
+
+	namedWindow("image");
+	imshow("image", img);
+	vector<int> params;
+	params.push_back(IMWRITE_JPEG_QUALITY);
+	params.push_back(95);
+	imwrite("opencv_gilbut\\helloCV\\lenna1.jpg", img, params);
+
+	waitKey();
+	destroyWindow("image");
+
+	return 0;
+}
