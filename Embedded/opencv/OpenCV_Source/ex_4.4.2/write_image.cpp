d224fc6325a4efa8c6799c8d5969520a7d8a0a56
chi <cldud97@naver.com>
2025-07-25 11:43:51 +0900

    경로 변경

--- Embedded/opencv/OpenCV_Source/ex_4.4.2/write_image.cpp
+++ Embedded/opencv/OpenCV_Source/ex_4.4.2/write_image.cpp
@@ -3,18 +3,18 @@ using namespace cv;
 using namespace std;
 int main()
 {
-	Mat img8 = imread("../image/read_color.jpg", IMREAD_COLOR);
-	CV_Assert(img8.data);
+    Mat img8 = imread("Opencv_Source/image/read_color.jpg", IMREAD_COLOR);
+    CV_Assert(img8.data);
 
-	vector<int> params_jpg, params_png;
-	params_jpg.push_back(IMWRITE_JPEG_QUALITY);
-	params_jpg.push_back(50);
-	params_png.push_back(IMWRITE_PNG_COMPRESSION);
-	params_png.push_back(9);
+    vector<int> params_jpg, params_png;
+    params_jpg.push_back(IMWRITE_JPEG_QUALITY);
+    params_jpg.push_back(50);
+    params_png.push_back(IMWRITE_PNG_COMPRESSION);
+    params_png.push_back(9);
 
-	imwrite("../image/write_test1.jpg", img8);
-	imwrite("../image/write_test2.jpg", img8, params_jpg);
-	imwrite("../image/write_test.png", img8, params_png);
-	imwrite("../image/write_test.bmp", img8);
-	return 0;
+    imwrite("OpenCV_Source/image/write_test1.jpg", img8);
+    imwrite("OpenCV_Source//image/write_test2.jpg", img8, params_jpg);
+    imwrite("OpenCV_Source/image/write_test.png", img8, params_png);
+    imwrite("OpenCV_Source/image/write_test.bmp", img8);
+    return 0;
 }
