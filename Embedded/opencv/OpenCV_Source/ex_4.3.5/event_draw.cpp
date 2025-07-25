dd33ecf8d20c0848b66b8fe6a60f3c1fddc46bfa
chi <cldud97@naver.com>
2025-07-25 12:27:39 +0900

    이미지 저장하는 코드 추가

--- Embedded/opencv/OpenCV_Source/ex_4.3.5/event_draw.cpp
+++ Embedded/opencv/OpenCV_Source/ex_4.3.5/event_draw.cpp
@@ -51,7 +51,19 @@ int main()
     image = Mat(300, 500, CV_8UC3, Scalar(255, 255, 255));
     imshow(title, image);
     setMouseCallback(title, onMouse, 0);
-
     waitKey(0);
+    vector<int> params_jpg, params_png, params_bmp;
+    params_jpg.push_back(IMWRITE_JPEG_QUALITY);
+    params_jpg.push_back(50);
+    params_png.push_back(IMWRITE_PNG_COMPRESSION);
+    params_png.push_back(9);
+    params_bmp.push_back(IMWRITE_PXM_BINARY);
+    params_bmp.push_back(0);
+    imwrite("OpenCV_Source\\ex_4.3.5\\event_draw.cppevent_draw.jpg", image);
+    imwrite("OpenCV_Source\\ex_4.3.5\\event_draw.cppevent_draw2.jpg", image, params_jpg);
+    imwrite("OpenCV_Source\\ex_4.3.5\\event_draw.cppevent_darw.png", image);
+    imwrite("OpenCV_Source\\ex_4.3.5\\event_draw.cppevent_draw2.png", image, params_png);
+    imwrite("OpenCV_Source\\ex_4.3.5\\event_draw.cppevent_draw.bmp", image);
+    imwrite("OpenCV_Source\\ex_4.3.5\\event_draw.cppevent_draw2.bmp", image, params_bmp);
     return 0;
 }
